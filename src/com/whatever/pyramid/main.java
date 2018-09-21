7bf37011ae3f79107878050b738dcd084a2e0d9b
Brian <centerflag982@yahoo.com>
2018-09-21 01:53:13 -0500

    Added file output option

--- src/com/whatever/pyramid/main.java
+++ src/com/whatever/pyramid/main.java
@@ -1,11 +1,14 @@
 package com.whatever.pyramid;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 public class main {
     private static Scanner inputReader = new Scanner(System.in);
     public static void main(String [ ] args) {
         int inputInt = getInput();
-        drawPyramid(inputInt);
+        int outputTypeInt = getOutputType();
+        drawPyramid(inputInt, outputTypeInt);
     }
 
     private static int getInput(){
@@ -17,7 +20,25 @@ public class main {
         return input;
     }
 
-    private static void drawPyramid(int rows){
+    private static int getOutputType(){
+        int output = -1;
+        while (output < 1 || output > 3) {
+            System.out.println("Enter 1 to output to console, or 2 to output to file.");
+            output = inputReader.nextInt();
+        }
+        return output;
+    }
+
+    private static void drawPyramid(int input, int outputType){
+        if (outputType == 1){
+            drawPyramidToConsole(input);
+        }
+        else{
+            drawPyramidToFile(input);
+        }
+    }
+
+    private static void drawPyramidToConsole(int rows){
         for (int i = 0; i < rows; i++){
             for (int space = (rows - i); space >= 0; space--){
                 System.out.print(" ");
@@ -26,6 +47,25 @@ public class main {
                 System.out.print("#");
             }
             System.out.print("\n");
+        }
+    }
+
+    private static void drawPyramidToFile(int rows){
+        try {
+            PrintWriter outputWriter = new PrintWriter("pyramidOutput.txt");
+            for (int i = 0; i < rows; i++){
+                for (int space = (rows - i); space >= 0; space--){
+                    outputWriter.append(" ");
+                }
+                for (int pound = 0; pound < i + 2; pound++){
+                    outputWriter.append("#");
+                }
+                outputWriter.println();
+            }
+            outputWriter.close();
+        }
+        catch (IOException e) {
+            //Everything is fine, nothing to see here, move along
         }
     }
 }
